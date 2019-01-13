7ef3b44c6892be92608be4067e6f548adf23fdee
weaming <garden.yuen@gmail.com>
2019-01-13 13:11:56 +0800

    add decorator on colors

--- simple_colors.py
+++ simple_colors.py
@@ -1,4 +1,7 @@
-version = "0.1.0"
+"""
+https://github.com/fatih/color/blob/master/color.go<Paste>
+"""
+version = "0.1.1"
 __all__ = ["black", "red", "green", "yellow", "blue", "magenta", "cyan"]
 
 
@@ -17,12 +20,30 @@ def get_color_code(color):
     return locals().get(color.upper())
 
 
+formats = {
+    "bright": 1,
+    "bold": 1,
+    "dim": 2,
+    "italic": 3,
+    "underlined": 4,
+    "blink": 5,
+    "reverse": 7,
+}
+
+
 def get_color_func(color):
-    def fn(text):
+    def fn(text, format=None):
+        if isinstance(format, str):
+            format = formats[format]
+        assert format in list(range(1, 9))
+
         code = get_color_code(color)
         if not code:
             raise Exception("color {} not support".format(color))
-        return "\033[{}m{}\033[0m".format(code, text)
+        if format:
+            return "\033[{};{}m{}\033[0m".format(format, code, text)
+        else:
+            return "\033[{}m{}\033[0m".format(format, code, text)
 
     return fn
 
